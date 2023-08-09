4fe117a9dd91f4bedd96540667a45faa68ce253c
Ali Musthofa <alfaonline13@gmail.com>
2023-08-09 08:34:18 +0700

    Create latihan1.php

---
+++ html/latihan1.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<!--
+Saya adalah Komentar HTML tidak bisa di baca di Web Browser
+-->
+<head>
+    <!-- title untuk judul -->
+    <title>PHP in HTML</title>
+</head>
+<!-- body atau area layar -->
+<body>
+    <?php
+        echo "Hai PHP";
+    ?>
+    <br>
+    <h1>Heading 1</h1>
+    <table>
+        <tr>
+            <td>Kolom 1 Baris 1</td>
+            <td>Kolom 2 Baris 1</td>
+            <td>Kolom 3 Baris 1</td>
+        </tr>
+        <tr>
+            <td>Nama</td>
+            <td> : </td>
+            <td>
+                <?php echo "Ali Musthofa"; ?>
+            </td>
+        </tr>
+        <tr>
+            <td>Kelas</td>
+            <td> : </td>
+            <td>
+                <?php echo "berapa"; ?>
+            </td>
+        </tr>
+        <tr>
+            <td>No. Wa</td>
+            <td> : </td>
+            <td>
+                <?php echo "000"; ?>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
