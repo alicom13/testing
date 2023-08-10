11577d08e82a69aba931932f40998b2e1dfb4b21
Ali Musthofa <alfaonline13@gmail.com>
2023-08-10 08:36:21 +0700

    Update tambah.php

--- php/tambah.php
+++ php/tambah.php
@@ -1,3 +1,34 @@
-<?php
-echo 'saya echo';
-?>
+<html>
+  <head>
+    <title>CRUD</title>
+
+    <style>
+      ul {
+        list-style: none;
+      }
+      li {
+        display: inline;
+      }
+    </style>
+  </head>
+  <body>
+    <header>
+      Inggil
+    </header>
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Lihat</li>
+        <li>Tambah</li>
+        <li>Edit</li>
+        <li>Hapus</li>
+      </ul>
+    </nav>
+    <main>
+      
+    </main>
+    <footer>
+      Andap
+    </footer>
+  </body>
+</html>
