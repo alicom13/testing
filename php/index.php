f98c9eaaef898d53bcac3396cc795311438cf608
Ali Musthofa <alfaonline13@gmail.com>
2023-08-10 08:26:08 +0700

    Create index.php

---
+++ php/index.php
@@ -0,0 +1,34 @@
+<html>
+  <head>
+    <title>CRUD</title>
+
+    <style>
+      ul {
+        list-style: none;
+      }
+      li {
+        display: inline;
+      }
+    </style>
+  </head>
+  <body>
+    <header>
+      Inggil
+    </header>
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Lihat</li>
+        <li>Tambah</li>
+        <li>Edit</li>
+        <li>Hapus</li>
+      </ul>
+    </nav>
+    <main>
+      Tengah
+    </main>
+    <footer>
+      Andap
+    </footer>
+  </body>
+</html>
